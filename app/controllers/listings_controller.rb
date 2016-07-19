9d5ccdad4492394964b65568111c4ccde4b8e44d
jayloh90 <jayloh.dev@gmail.com>
2016-07-19 16:17:03 +0800

    Completed Facebook Login

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,4 @@
 class ListingsController < ApplicationController
-
 	def index
-		@listings = Listing.all
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719062439) do
+ActiveRecord::Schema.define(version: 20160719073141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string "uid"
+    t.string "token"
+    t.string "provider"
+    t.string "user_id"
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
